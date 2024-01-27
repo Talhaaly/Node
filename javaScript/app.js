1c045f673b630bde783179448de7f799bc3cf1c1
Talha <talhaaly103@gmail.com>
2024-01-27 22:29:40 +0600

    js

---
+++ javaScript/app.js
@@ -0,0 +1,102 @@
+
+
+// const obj = {
+//     name : "talha",
+//     class: "web&app",
+//     age: 23
+// }
+
+
+// console.log(obj , '---->');
+
+// setTimeout
+
+
+// const start = document.getElementById('start');
+// const stop = document.getElementById('stop');
+// const showName = document.querySelector('#showName')
+
+
+// const showMyName = () => {
+//     showName.innerHTML ="loading..."
+//     setTimeout(() => {
+//         showName.innerHTML = "Talha Ali Akber"
+//     }, 2000);
+// }
+
+// start.addEventListener('click', showMyName)
+
+
+// function myFunction(params) {
+//     alert("welcome Talha")
+// }
+
+
+// const stopNum = document.querySelector("p")
+// const stopBtn = document.querySelector("#btn")
+// const stopBtn2 = document.querySelector("#btnn")
+
+// let num = 100;
+// let timeRef;
+
+// const stop = ()=> {
+//     stopNum.innerHTML = "Loading..."
+
+//     timeRef =  setInterval(() => {
+//         stopNum.innerHTML = `${num}`;
+//         num--
+//     }, 1000);
+// }
+
+// stopBtn.addEventListener("click", stop)
+
+// stopBtn2.addEventListener('click', ()=> {
+//     clearInterval(timeRef)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
