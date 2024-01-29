45cf3cc5fa70ded56c7ccee806eaef883141fc6e
Talha <talhaaly103@gmail.com>
2024-01-30 00:00:40 +0600

    promises

--- promises/promise.js
+++ promises/promise.js
@@ -20,17 +20,42 @@
 // })
 
 
+// const promise_3 = new Promise(function(resolve , reject) {
+//     setTimeout(function(){
+//         let error = true;
+//         // let error = false;
+//         if(!error){
+//             resolve({name: "Talha Ali" , pass: "123456"})
+//         }else{
+//             reject('ERROR: js went wrong')
+//         }
+//     },1000)
+// })
+
+// async function consumePromise_3 () {
+//    try {
+//     const response = await promise_3
+//     console.log(response);
+//    } catch (error) {
+//     console.log(error);
+//    }
+// }
+
+// consumePromise_3()
 
 
 
 
-
-
-
-
-
-
-
+// fetch('https://maroon-shorts.cyclic.cloud/api/filterations/all')
+// .then((response)=> {
+// return response.json()
+// })
+// .then((data)=> {
+//     console.log(data);
+// })
+// .catch((error)=> {
+//     console.log('E', error);
+// })
 
 
 
